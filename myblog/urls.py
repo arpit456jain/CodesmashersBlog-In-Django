e6954d1ff2dcdf83d1d1905e935338e7449f47d7
Arpit <111arpit1@gmail.com>
2021-07-29 19:00:59 +0530

    update

--- myblog/urls.py
+++ myblog/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('myblog/', include('home.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
